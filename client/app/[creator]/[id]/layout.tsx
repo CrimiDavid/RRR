5f72a08177911795f3a04e3752cc7a8d2a6f41e8
David Crimi <crimid01@gmail.com>
2025-09-02 17:00:38 -0400

    added layout for small devices

---
+++ client/app/[creator]/[id]/layout.tsx
@@ -0,0 +1,9 @@
+import DiscoverLayout from "@/app/discover/layout";
+
+export default function PostLayout({children}) {
+    return (
+        <DiscoverLayout>
+            {children}
+        </DiscoverLayout>
+    )
+}
--- client/app/[creator]/[id]/page.tsx
+++ client/app/[creator]/[id]/page.tsx
@@ -1,12 +1,19 @@
 import ResizablePanels from "@/components/posts/post-page";
+import ResizablePanelsSmall from "@/components/posts/post-page-small";
 
 export default async function Post({params}) {
     await new Promise((res) => setTimeout(res, 2000));
     const {creator, id} = await params
     return (
-        <>
-            <ResizablePanels/>
-        </>
+        <div>
+            <div className="block lg:hidden">
+                <ResizablePanelsSmall/>
+            </div>
+
+            <div className="hidden lg:block">
+                <ResizablePanels/>
+            </div>
+        </div>
 
     )
 }
